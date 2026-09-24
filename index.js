090a25339d3037f93f69c166ccd4b0ee1dffa3cb
blessboydach <blessboydach@gmail.com>
2026-09-24 21:38:58 +0300

    Add HTTP server to index.js

--- index.js
+++ index.js
@@ -21,6 +21,9 @@
 
 'use strict'
 
+require('http').createServer((_, res) => { res.writeHead(200); res.end('ok') })
+  .listen(process.env.PORT || 3000)
+
 const fs     = require('fs')
 const path   = require('path')
 const https  = require('https')
